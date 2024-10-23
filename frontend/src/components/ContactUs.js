39af6f42ad5bf5ae1eae253dd620154e6cd729f3
Ranjith-41 <ranjithkittusamy.com>
2024-10-23 22:07:58 +0530

    updated

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import Map from './Map';
 import './ContactUs.css';
