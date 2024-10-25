7fcf08c00678862173690370bc583011abd25866
Ranjith-41 <110477306+Ranjith-41@users.noreply.github.com>
2024-10-25 18:46:14 +0530

    Update carousel.jsx

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -14,7 +14,7 @@ const ReviewsCarousel = () => {
   useEffect(() => {
     const fetchFeedbacks = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/feedback'); // Your backend endpoint
+        const response = await axios.get('https://avani-smart-clinic.onrender.com/feedback'); // Your backend endpoint
         setFeedbacks(response.data);
       } catch (error) {
         console.log('Error fetching feedback:', error);
